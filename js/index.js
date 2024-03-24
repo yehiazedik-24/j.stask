cdaf71d4398087a1b77594edba6ee4a0e0b11287
yehiazedik-24 <yehiacool2013@gmail.com>
2024-03-25 00:15:26 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+
+
+alert("لايجاد مساحة ومحيط مستطيلك ");
+var x =Number(prompt("ادخل طول الضلع"));
+var y =Number(prompt("ادخل عرض الضلع"));
+
+var area = x*y
+var premater = 2* (x + y);
+console.log(area);
+console.log(premater);
